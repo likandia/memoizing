0675d13061386bf613d51feddc212237d6be7d5e
P.C. Sruthi <likandia@gmail.com>
2015-11-20 22:37:17 +0530

    Add version 3 - with templates, but single argument

--- memo.cpp
+++ memo.cpp
@@ -9,6 +9,24 @@ int factorial(int n){
       return (n * factorial(n-1));
 }
 
+//Version 3
+template<class R, class A>
+std::function <R (A)> memoize(std::function<R (A)> f){
+  std::function <R (A)> memf;
+  memf = [&f](A n){
+    static std::map<A, R> cache;
+    if(cache.find(n) != cache.end()){
+      return cache[n];
+    }
+
+    R a = f(n);
+    cache.insert(std::pair<A, R>(n, a));
+    return a;
+  };
+  return memf;
+}
+
+
 int main(int argc, char **argv){
   //Version 1
   std::function <int (int)> memfact = [](int n){
@@ -42,6 +60,9 @@ int main(int argc, char **argv){
     return a;
   };
 
+  //Version 3
+  std::function<int (int)> memfactt = memoize((std::function<int(int)>)factorial);
+
   //Tests for 1
   std::cout << "Version 1: No templates - does not work with recursion" << std::endl;
   std::cout << memfact(5) << std::endl << std::endl;
@@ -55,5 +76,12 @@ int main(int argc, char **argv){
   std::cout << memfactr(10) << std::endl << std::endl;
   std::cout << memfactr(6) << std::endl << std::endl;
   std::cout << memfactr(10) << std::endl << std::endl;
+
+  //Tests for 3
+  std::cout << "Version 3: Using templates for single argument and returned value - does not work with recursion" << std::endl;
+  std::cout << memfactt(5) << std::endl << std::endl;
+  std::cout << memfactt(10) << std::endl << std::endl;
+  std::cout << memfactt(6) << std::endl << std::endl;
+  std::cout << memfactt(10) << std::endl << std::endl;
   return 0;
 }
